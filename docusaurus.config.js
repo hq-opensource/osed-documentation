f3367b286a068b25fbd5c0625bea18842edf92c2
juancaoviedo <ovi.sa@hotmail.com>
2024-08-08 16:43:44 -0400

    change config on main

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,16 +13,16 @@ const config = {
   favicon: 'img/thunder.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://hq-opensource.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'hq-opensource', // Usually your GitHub org/user name.
+  projectName: 'osed-docs', // Usually your repo name.
+  trailingSlash: false,
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
